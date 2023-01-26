a6fba8c4186b23d39cb2b366413ddfbe00a98c85
Michael <hovgaard1990@gmail.com>
2023-01-26 12:18:45 +0100

    add

--- veveve/next.config.js
+++ veveve/next.config.js
@@ -6,25 +6,11 @@ const nextConfig = {
 module.exports = nextConfig;
 
 // next.config.js
-
-// You can choose which headers to add to the list
-// after learning more below.
-const securityHeaders = [];
+const helmet = require("helmet");
 
 module.exports = {
-  async headers() {
-    return [
-      {
-        // Apply these headers to all routes in your application.
-        source: "/:path*",
-        headers: securityHeaders,
-        //X-frame-Option implemented in _app.tsx; test needed
-        key: "X-Frame-Options",
-        value: "SAMEORIGIN",
-        // X-XSS-protection // not in use yet
-        key: "X-XSS-Protection",
-        value: "1; mode=block",
-      },
-    ];
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    headers: helmet.frameguard({ action: "sameorigin" }),
   },
 };
