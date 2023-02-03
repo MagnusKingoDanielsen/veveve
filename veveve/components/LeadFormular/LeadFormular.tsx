80d4890731c4a4242bc7ba9d88b4b781abd66d8a
MagnusKingoDanielsen <magnus@metteogivar.dk>
2023-02-03 09:30:19 +0100

    added new features
    Added a value to answers, hot lead value and display none styling if answers don't have  3 answers.

--- veveve/components/LeadFormular/LeadFormular.tsx
+++ veveve/components/LeadFormular/LeadFormular.tsx
@@ -6,6 +6,8 @@ import style from "../../styles/LeadFormular.module.css";
 const questions: React.FC = () => {
   const [question, setQuestions] = useState<LFQ[]>([]);
   const [activeQuestionNumber, setActiveQuestionNumber] = useState<number>(0);
+  const [leadFormularValue, setLeadFormularValue] = useState(0);
+  const [hotLeadValue, setHotLeadValue] = useState(0);
 
   useEffect(() => {
     fetch(process.env.NEXT_PUBLIC_BASEPATH + "api/LeadFormular")
@@ -17,13 +19,19 @@ const questions: React.FC = () => {
   }, []);
   function setValue(x: number) {
     if (activeQuestionNumber + x > question.length - 1) {
-      setActiveQuestionNumber(0);
+      setActiveQuestionNumber(question.length - 1);
+      setHotLeadValue(hotLeadValue + Number(leadFormularValue));
     } else if (activeQuestionNumber + x < 0) {
-      setActiveQuestionNumber(question.length + x);
+      setActiveQuestionNumber(0);
     } else {
       setActiveQuestionNumber(activeQuestionNumber + x);
+      setHotLeadValue(hotLeadValue + Number(leadFormularValue));
     }
   }
+
+  const onOptionChange = (e) => {
+    setLeadFormularValue(e.target.value);
+  };
 
   return (
     <>
@@ -38,6 +46,7 @@ const questions: React.FC = () => {
                     type="radio"
                     name="lead_formular"
                     value={questions.answer1Value}
+                    onChange={onOptionChange}
                   />
                   <label>{questions.answer1}</label>
                 </div>
@@ -46,18 +55,26 @@ const questions: React.FC = () => {
                     type="radio"
                     name="lead_formular"
                     value={questions.answer2Value}
+                    onChange={onOptionChange}
                   />
                   <label>{questions.answer2}</label>
                 </div>
-                <div>
+                <div
+                  className={
+                    questions.answer3 == null ? style.none : style.show
+                  }
+                >
                   <input
                     type="radio"
                     name="lead_formular"
                     value={questions.answer3Value}
+                    onChange={onOptionChange}
                   />
 
                   <label>{questions.answer3}</label>
                 </div>
+                <p>{leadFormularValue}</p>
+                <p>{hotLeadValue}</p>
                 <button onClick={() => setValue(-1)}>tilbage</button>
                 <button onClick={() => setValue(1)}>fremad</button>
               </div>
